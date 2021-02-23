7a56f6b0d481bde5919ba4158e2f7ab20482bd2c
Shirley Smajd <smajd.shirley@principal.com>
2021-02-23 16:19:14 -0600

    added Bowling class, constructor, this, if

--- bowling/src/index.js
+++ bowling/src/index.js
@@ -1,10 +1,27 @@
-let bucket = 0
-function roll(pins) {
-  bucket += pins;
+import { getRoles } from "@testing-library/react";
+
+class Bowling {
+  constructor() {
+    this.bucket = 0;
+    this.rolls = new Array();
+  }
+
+  roll(pins) {
+    this.rolls.push(pins);
+  }
+
+  score() {
+    for (let rollIndex = 0; rollIndex < 20; rollIndex++) {
+      if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10)
+      
+      {
+
+      } //add else stmt
+      this.bucket += this.rolls[rollIndex];
+    }
+
+    return this.bucket;
+  }
 }
 
-function score() {
-  return bucket;
-}
-
-export {roll, score}
+export { Bowling }
--- bowling/src/index.test.js
+++ bowling/src/index.test.js
@@ -1,37 +1,44 @@
-import { roll, score } from './index.js';
+import { Bowling } from './index.js';
+
 test("gutter game returns zero", () => {
+    //arrange
+    let game = new Bowling();
     for (let i = 0; i < 20; i++) {
-        roll(0);
+        game.roll(0);
     }
-
-    let actual = score();
-
+    //act
+    let actual = game.score();
+    //assert
     expect(actual).toBe(0);
 });
 
 test("one open frame and all rest gutters returns sum of its pins knocked down", () => {
-    roll(2);
-    roll(1);
-    roll(3);
-    roll(4);
+    //arrange
+    let game = new Bowling();
+    game.roll(2);
+    game.roll(1);
+    game.roll(3);
+    game.roll(4);
     for (let i = 0; i < 16; i++) {
-        roll(0);
+        game.roll(0);
     }
-
-    let actual = score();
-
+    //act
+    let actual = game.score();
+    //assert
     expect(actual).toBe(10);
 });
 
 test("spare returns 10 plus the next roll", () => {
-    roll(6);
-    roll(4);
-    roll(5);
+    //arrange
+    let game = new Bowling();
+    game.roll(6);
+    game.roll(4);
+    game.roll(5);
     for (let i = 0; i < 17; i++) {
-        roll(0);
-
+        game.roll(0);
     }
-    let actual = score();
-
+    //act
+    let actual = game.score();
+    //assert
     expect(actual).toBe(20);
 });
